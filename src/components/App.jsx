0c7a0f9f074f614b8bf034d34a4fb4cb5ebe93e9
Eduard Antoshchuk <e.antoshchuk@gmail.com>
2023-05-09 01:38:44 +0300

    hw-2-phonebook-task2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,36 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React-hw-02-phonebook
-    </div>
-  );
-};
+import { Component } from 'react';
+import PhoneBook from './Phonebook/Phonebook';
+
+class App extends Component {
+  state = {
+    contacts: [],
+  };
+
+  formSubmitHandler = data => {
+    console.log(data);
+  };
+
+  render() {
+    return (
+      <div>
+        <div
+          style={{
+            height: '100px',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            fontSize: 40,
+            color: '#010101',
+          }}
+        >
+          React-hw-02-phonebook
+        </div>
+        <div>
+          <PhoneBook onSubmit={this.formSubmitHandler} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
